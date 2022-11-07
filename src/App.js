8a2bcc6e2a6b4b22483b547ac1be89d5e113030e
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-11-07 11:25:50 -0500

    Expansion working. Fixed null data points. Styled buttons.

--- src/App.js
+++ src/App.js
@@ -7,24 +7,52 @@ function App({ bookData }) {
         <div>
             <h1>Freeshelf</h1>
             {bookData.map((data) => (
-                <div className="bookCard">
-                    <ul>
-                        <li>{data.title}</li>
-                        <li>{data.author}</li>
-                        <li>{data.shortDescription}</li>
-                    </ul>
-                        
+                <div className="wrapper">
+                    <div className="imageLoc">
+                        <img src={data.coverImageUrl} alt="cover"/>
+                    </div>
+                    <div className="bookInfo">
+                        <ul>
+                            <li><h3>{data.title}</h3></li>
+                            <li>by {data.author}</li>
+                            <li><p>{data.shortDescription}</p></li>
+                            <li><ExpandedInfo 
+                                url={data.url} 
+                                publisher={data.publisher} 
+                                pubDate={data.publicationDate} 
+                                detailedDesc={data.detailedDescription}/>
+                            </li>
+                        </ul>
+                            
+                    </div>
                 </div>
             ))}
-            
-            
         </div>
     );
 }
 
 
-function expandedInfo({url, publisher, pubDate, detailedDesc}){
-    const [expansion, setExpansion] = useState(false);
+function ExpandedInfo({url, publisher, pubDate, detailedDesc}){
+    const [isExpanded, setExpansion] = useState(false);
+    const buttonName = isExpanded ? "Less" : "More";
+
+    return (
+        <div>
+            <button onClick={() => setExpansion(!isExpanded)}><strong>{buttonName} Info</strong></button>
+            {isExpanded && (
+                <div className="expandedBox">
+                    <h4>From the Back Cover:</h4>
+                    <p className="longDesc">{detailedDesc}</p>
+                    {publisher ? <p><strong>Publisher:</strong> {publisher}</p> : null}
+                    {pubDate ? <p><strong>Published:</strong> {pubDate}</p> : null}
+                    {url ? <a href={url} target="_blank" rel="noreferrer"><button><strong>Get Book</strong></button></a> : null}
+
+                </div>
+                
+            )}
+        </div>
+        
+    )
     
 
 }
